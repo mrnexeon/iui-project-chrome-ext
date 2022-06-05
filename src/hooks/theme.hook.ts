594a66b14273bf5481e0a56bb319e8031bceb456
Lukas Thiersh <lukas.thiersch@lansco.de>
2022-06-05 11:30:50 +0200

    Added Theming

---
+++ src/hooks/theme.hook.ts
@@ -0,0 +1,41 @@
+import { createTheme, Theme } from '@mui/material';
+import * as React from 'react';
+
+const lightTheme = createTheme({
+    palette: {
+        mode: 'light',
+    },
+});
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+/**
+ * Checks the user's preferred mode and returns a mui theme accordingly
+ * Will update when when mode changes
+ *
+ * @returns Theme theme in dark or light mode
+ */
+export const usePreferredTheme = (): Theme => {
+    const [theme, setTheme] = React.useState(lightTheme);
+
+    React.useEffect(() => {
+        const isDarkMode = window.matchMedia(
+            '(prefers-color-scheme: dark)',
+        ).matches;
+
+        setTheme(isDarkMode ? darkTheme : lightTheme);
+
+        window
+            .matchMedia('(prefers-color-scheme: dark)')
+            .addEventListener('change', (e) => {
+                console.log(e);
+                setTheme(e.matches ? darkTheme : lightTheme);
+            });
+    }, []);
+
+    return theme;
+};
--- src/pages/popup/App.tsx
+++ src/pages/popup/App.tsx
@@ -1,11 +1,35 @@
+import {
+    Box,
+    CssBaseline,
+    FormControlLabel,
+    Switch,
+    ThemeProvider,
+    Typography,
+} from '@mui/material';
 import React from 'react';
+import { usePreferredTheme } from '../../hooks/theme.hook';
 
 const App = (): JSX.Element => {
+    const theme = usePreferredTheme();
+
     return (
-        <div>
-            <h1>Popup Page</h1>
-            <p>If you are seeing this, React is working!</p>
-        </div>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box
+                sx={{
+                    minWidth: '300px',
+                    backgroundColor: theme.palette.background.default,
+                    padding: '15px',
+                }}
+                textAlign="center"
+            >
+                <Typography variant="h4">YouLearn</Typography>
+                <FormControlLabel
+                    control={<Switch defaultChecked />}
+                    label="Filter Recommendations"
+                />
+            </Box>
+        </ThemeProvider>
     );
 };
 
