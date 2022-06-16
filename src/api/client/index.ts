b448fe02d353e0b000d2851fefc97bfdc3adedd6
Andrei Arkhipov <the.nexeon@gmail.com>
2022-06-16 12:20:49 +0200

    Restructuring and commenting REST API functions

--- src/api/client/index.ts
+++ src/api/client/index.ts
@@ -1,11 +1,38 @@
-import { Api, HttpResponse } from './client';
+import { Api } from './client';
 
 const client = new Api();
 
-export function postFeedback(video_id: string, distractful: boolean) : Promise<HttpResponse<void, any>> {
-    return client.api.feedbackCreate({ video_id, distractful })
+/**
+ * Reports a feedback about the filtered video to REST API server
+ * 
+ * @param video_id YouTube video ID
+ * @param distractful true if the video is indeed distractful, false if the video should not be filtered out
+ * @returns true if the report has been recieved by the server
+ */
+export function reportFeedback(video_id: string, distractful: boolean): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        client.api.feedbackCreate({ video_id, distractful })
+            .then(res => resolve(res.status === 200))
+            .catch(err => reject(err))
+    })
 }
 
-export function filterVideos(videoIds: string[]) : Promise<HttpResponse<{distractful_ids: string[] }, string>> {
-    return client.api.filterDistractfulVideosCreate({ recommended_ids: videoIds });
+/**
+ * Requests REST API server to filter educational videos from the video list
+ * 
+ * @param videoIds YouTube videos IDs list
+ * @returns educational videos IDs for displaying which are *not* distractful
+ */
+export function filterEducationalVideos(videoIds: string[]): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+
+        // NB! the Endpoint name in REST API spec is not correct from the meaning, 
+        // the endpoint does not filters distractful, but filters educational videos
+
+        client.api.filterDistractfulVideosCreate({ recommended_ids: videoIds })
+            .then(res => res.json())
+            .catch(err => reject(err))
+            .then(json => resolve(json.distractful_ids))
+            .catch(err => reject(err))
+    })
 }
